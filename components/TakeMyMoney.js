612d064c2c905d8c22745af1d4f87a4781dace1b
C Baucom <chrisrb83@gmail.com>
2018-11-21 01:20:34 -0500

    Charging credit cards on the server side

--- components/TakeMyMoney.js
+++ components/TakeMyMoney.js
@@ -9,38 +9,68 @@ import calcTotalPrice from "../lib/calcTotalPrice";
 import Error from "./ErrorMessage";
 import User, { CURRENT_USER_QUERY } from "./User";
 
+const CREATE_ORDER_MUTATION = gql`
+  mutation createOrder($token: String!) {
+    createOrder(token: $token) {
+      id
+      charge
+      total
+      items {
+        id
+        title
+      }
+    }
+  }
+`;
+
 function totalItems(cart) {
   return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
 }
 
 class TakeMyMoney extends React.Component {
-  onToken = res => {
+  onToken = (res, createOrder) => {
     console.log("On Token Called!");
     console.log(res);
+    createOrder({
+      variables: {
+        token: res.id
+      }
+    }).catch(err => {
+      alert(err.message);
+    });
   };
   render() {
     return (
       <User>
         {({ data: { me } }) => (
-          <StripeCheckout
-            amount={calcTotalPrice(me.cart)}
-            name="New Navy" // the pop-in header title
-            description={`Order of ${totalItems(me.cart)} items!`} // the pop-in header subtitle
-            image={me.cart.length && me.cart[0].item && me.cart[0].item.image} // the pop-in header image (default none)
-            stripeKey="pk_test_eu9jxjHPRi448DrKOoGDpNs8"
-            currency="USD"
-            email={me.email}
-            // Note: Enabling either address option will give the user the ability to
-            // fill out both. Addresses are sent as a second parameter in the token callback.
-            // shippingAddress
-            // billingAddress={false}
-            // Note: enabling both zipCode checks and billing or shipping address will
-            // cause zipCheck to be pulled from billing address (set to shipping if none provided).
-            // zipCode={false}
-            token={res => this.onToken(res, createOrder)}
+          <Mutation
+            mutation={CREATE_ORDER_MUTATION}
+            refetchQueries={[{ query: CURRENT_USER_QUERY }]}
           >
-            {this.props.children}
-          </StripeCheckout>
+            {createOrder => (
+              <StripeCheckout
+                amount={calcTotalPrice(me.cart)}
+                name="New Navy" // the pop-in header title
+                description={`Order of ${totalItems(me.cart)} items!`} // the pop-in header subtitle
+                image={
+                  me.cart.length && me.cart[0].item && me.cart[0].item.image
+                } // the pop-in header image (default none)
+                stripeKey="pk_test_eu9jxjHPRi448DrKOoGDpNs8"
+                currency="USD"
+                email={me.email}
+                // Note: Enabling either address option will give the user the ability to
+                // fill out both. Addresses are sent as a second parameter in the token callback.
+                // shippingAddress
+                // billingAddress={false}
+                // Note: enabling both zipCode checks and billing or shipping address will
+                // cause zipCheck to be pulled from billing address (set to shipping if none provided).
+                // zipCode={false}
+                token={res => this.onToken(res, createOrder)}
+              >
+                {this.props.children}
+              </StripeCheckout>
+            )}
+          </Mutation>
         )}
       </User>
     );
