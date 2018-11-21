4fc28c43cd41730782bdd3bf527d6d693d90d647
C Baucom <chrisrb83@gmail.com>
2018-11-21 00:41:45 -0500

    fix broken emoji

--- components/AddToCart.js
+++ components/AddToCart.js
@@ -25,7 +25,7 @@ class AddToCart extends React.Component {
       >
         {(addToCart, { loading }) => (
           <button disabled={loading} onClick={addToCart}>
-            Add{loading && "ing"} To Cart 🛒
+            Add{loading && "ing"} To Cart
           </button>
         )}
       </Mutation>
